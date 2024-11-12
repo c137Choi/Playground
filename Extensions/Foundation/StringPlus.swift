6dba1146d8d733a44793fcf5f97f09deefc2b425
Choi <365473321@qq.com>
2024-11-12 21:21:11 +0800

    新增String扩展属性：判断是否包含emoji

--- Extensions/Foundation/StringPlus.swift
+++ Extensions/Foundation/StringPlus.swift
@@ -541,6 +541,35 @@ extension String {
 // MARK: - 转换
 extension String {
     
+    /// SwifterSwift: Check if string contains one or more emojis.
+    ///
+    ///        "Hello 😀".containEmoji -> true
+    ///
+    var containsEmoji: Bool {
+        // http://stackoverflow.com/questions/30757193/find-out-if-character-in-string-is-emoji
+        for scalar in unicodeScalars {
+            switch scalar.value {
+            case 0x1F600...0x1F64F, // Emoticons
+                0x1F300...0x1F5FF, // Misc Symbols and Pictographs
+                0x1F680...0x1F6FF, // Transport and Map
+                0x1F1E6...0x1F1FF, // Regional country flags
+                0x2600...0x26FF, // Misc symbols
+                0x2700...0x27BF, // Dingbats
+                0xE0020...0xE007F, // Tags
+                0xFE00...0xFE0F, // Variation Selectors
+                0x1F900...0x1F9FF, // Supplemental Symbols and Pictographs
+                127_000...127_600, // Various asian characters
+                65024...65039, // Variation selector
+                9100...9300, // Misc items
+                8400...8447: // Combining Diacritical Marks for Symbols
+                return true
+            default:
+                continue
+            }
+        }
+        return false
+    }
+    
     /// 中文->拼音. "中国"->"zhong guo"
     var pinyin: String {
         let pointer = NSMutableString(string: self)

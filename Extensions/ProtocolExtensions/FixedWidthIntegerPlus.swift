10b26461045b2c799e7c40eeb5fa3f9682af8ba4
Choi <365473321@qq.com>
2024-11-13 22:12:19 +0800

    优化整数转换二进制方法

--- Extensions/ProtocolExtensions/FixedWidthIntegerPlus.swift
+++ Extensions/ProtocolExtensions/FixedWidthIntegerPlus.swift
@@ -24,20 +24,28 @@ extension FixedWidthInteger {
         min...max
     }
     
-    /// 二进制
+    /// 二进制 | 小字节序读取二进制
     var data: Data {
-        dataInBytes()
+        data(byteOrder: .littleEndian)
     }
     
-    /// 转换为二进制
+    
+    /// 整数转换为二进制
     /// - Parameters:
-    ///   - byteCount: 占用字节数, 如不指定则使用自身默认占用的字节数
-    ///   - byteOrder: 字节序, 默认为小字节序
-    /// - Returns: 转换后的二进制对象(字节翻转过的数组: 从左到右为低位到高位排列)
-    /// 加注: 默认为小字节序: 二进制从左往右为数字的二进制从低位(右侧)到高位(左侧)按字节依次填充
-    func dataInBytes(_ preferredByteCount: Int? = nil, byteOrder: Data.ByteOrder = .littleEndian) -> Data {
-        var copy = byteOrder == .bigEndian ? bigEndian : littleEndian
-        let byteCount = preferredByteCount ?? bitWidth / 8
-        return Data(bytes: &copy, count: byteCount)
+    ///   - byteCount: 按照字节序读取的字节数
+    ///   - byteOrder: 字节序
+    /// - Returns: 读取的二进制
+    /// - 示例：UInt64(0xFF_FE_FD_FC_FB_FA_F9_F8) 小字节序-> [F8, F9, FA, FB, FC, FD, FE, FF]
+    /// - 示例: UInt64(0xFF_FE_FD_FC_FB_FA_F9_F8) 大字节序-> [FF, FE, FD, FC, FB, FA, F9, F8]
+    /// - 示例: UInt64(0xFD_FE_FF) 小字节序-> [FF, FE, FD, 0, 0, 0, 0, 0]
+    /// - 示例: UInt64(0xFD_FE_FF) 大字节序-> [0, 0, 0, 0, 0, FD, FE, FF]
+    /// - 示例: UInt32(0x00_FD_FE_FF) preferredByteCount==3, 小字节序 -> [FF, FE, FD]
+    /// - 示例: UInt32(0x00_FD_FE_FF) preferredByteCount==3, 大字节序 -> [00, FD, FE]
+    /// - 所以要注意使用的整数类型(UInt32/UInt16等)
+    /// - 并小心配合使用preferredByteCount和byteOrder, 因为方法本质是从低位或高位读取指定个字节的数据存到Data中
+    /// - 传入参数不当可能导致读取到错误的字节
+    func data(readingBytes byteCount: Int? = nil, byteOrder: Data.ByteOrder) -> Data {
+        var pointer = byteOrder == .bigEndian ? bigEndian : littleEndian
+        return Data(bytes: &pointer, count: byteCount ?? bitWidth / 8)
     }
 }

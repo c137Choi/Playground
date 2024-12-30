d667a9319e649af9b72210768847491ee7351b9f
Choi <365473321@qq.com>
2024-12-30 10:17:52 +0800

    新增UUID扩展

---
+++ Extensions/Foundation/UUIDPlus.swift
@@ -0,0 +1,16 @@
+//
+//  UUIDPlus.swift
+//  KnowLED
+//
+//  Created by Choi on 2024/12/30.
+//
+
+import Foundation
+
+extension UUID {
+    
+    /// 生成一个新的UUID
+    static var new: UUID {
+        UUID()
+    }
+}

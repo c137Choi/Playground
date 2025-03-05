03dbead2e607aedac89c689f012715018f77bba9
Choi <365473321@qq.com>
2025-03-05 09:07:18 +0800

    新增：lastIndex注释

--- Extensions/ProtocolExtensions/BidirectionalCollection.swift
+++ Extensions/ProtocolExtensions/BidirectionalCollection.swift
@@ -9,7 +9,7 @@ import Foundation
 
 extension BidirectionalCollection {
     
-    /// 最后一个Index | 如数组[1, 2, 3].lastIndex == 2
+    /// 最后一个合法的Index | 如数组[1, 2, 3].lastIndex == 2 | 空数组 [Int]().lastIndex == -1
     /// 注: 空的Range(例如: 0..<0)不要调用这个方法, 否则会崩溃, 使用maybeLastIndex替换
     var lastIndex: Index {
         index(before: endIndex)

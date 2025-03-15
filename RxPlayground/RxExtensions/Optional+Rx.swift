22ac9885906cd275401f70848f1b071a788115da
Choi <365473321@qq.com>
2025-03-15 16:51:04 +0800

    优化Optional+Rx方法

--- RxPlayground/RxExtensions/Optional+Rx.swift
+++ RxPlayground/RxExtensions/Optional+Rx.swift
@@ -20,21 +20,21 @@ extension Optional {
         }
     }
     
-    func assignUnwrapped(to binders: Binder<Wrapped>...) {
-        assignUnwrapped(to: binders)
-    }
-    func assignUnwrapped(to binders: [Binder<Wrapped>]) {
-        if case .some(let wrapped) = self {
-            binders.forEach { binder in
-                binder.onNext(wrapped)
-            }
-        }
-    }
-    
+    /// 只设置有效值
     func assignUnwrapped(to binders: Binder<Self>...) {
         assignUnwrapped(to: binders)
     }
     func assignUnwrapped(to binders: [Binder<Self>]) {
+        guard let unwrapped = self else { return }
+        binders.forEach { binders in
+            binders.onNext(unwrapped)
+        }
+    }
+    
+    func assign(to binders: Binder<Wrapped>...) {
+        assign(to: binders)
+    }
+    func assign(to binders: [Binder<Wrapped>]) {
         if case .some(let wrapped) = self {
             binders.forEach { binder in
                 binder.onNext(wrapped)

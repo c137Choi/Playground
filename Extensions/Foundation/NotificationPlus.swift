fea15e4f946ba075df329015f34e157fb804c4dc
Choi <365473321@qq.com>
2025-03-20 17:52:28 +0800

    新增：Notification.Name静态常量

--- Extensions/Foundation/NotificationPlus.swift
+++ Extensions/Foundation/NotificationPlus.swift
@@ -8,6 +8,11 @@
 import Foundation
 
 extension Notification.Name {
+    
+    /// 通知: 用户是否正在交互
+    static let isUserInteracting = Notification.Name.randomUUID
+    
+    /// 静态变量: 用随机UUID字符串生成一个Notification.Name
     static var randomUUID: Notification.Name {
         Notification.Name(.randomUUID)
     }

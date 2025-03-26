9a93cbebc8707ff6c55791262680637e26813fac
Choi <365473321@qq.com>
2025-03-26 11:45:25 +0800

    新增：UIView扩展属性

--- Extensions/UIKit/UIViewPlus.swift
+++ Extensions/UIKit/UIViewPlus.swift
@@ -107,6 +107,16 @@ extension UIView {
         @UniqueAddress static var tappedClosure
     }
     
+    var isVisibleAndPrepared: Bool {
+        isVisible && isPrepared
+    }
+    
+    /// 是否可见
+    /// 注: 只简单判断几个条件: 未隐藏\window属性有效\透明度超过阈值. 更复杂的层级结构需要根据情况具体判断
+    var isVisible: Bool {
+        !isHidden && window.isValid && alpha >= 0.01
+    }
+    
     /// 返回在Window中的frame
     var frameInWindow: CGRect {
         convert(bounds, to: nil)

affdc972740e38249ec6a620fd027b5f493548fa
Choi <365473321@qq.com>
2025-03-29 14:55:51 +0800

    修复：GCDTimer循环引用的问题

--- Extensions/Tools/GCDTimer.swift
+++ Extensions/Tools/GCDTimer.swift
@@ -10,40 +10,44 @@ import Foundation
 
 final class GCDTimer {
 	
-	typealias GCDTimerClosure = (GCDTimer) -> Void
+	typealias TickTock = (GCDTimer) -> Void
 	
 	/// GCD定时器
-	private var timerSource: DispatchSourceTimer?
+	private var _timer: DispatchSourceTimer?
 	
-	/// 返回有效的定时器
-	private var validTimer: DispatchSourceTimer {
-		guard let timer = timerSource else {
-			let source = DispatchSource.makeTimerSource(flags: .strict, queue: queue)
-			source.setEventHandler(handler: tickTock)
-			timerSource = source
-			return source
+	/// 返回定时器 | 如果不存在则创建一个
+	private var timer: DispatchSourceTimer {
+		guard let existingTimer = _timer else {
+			let babyTimer = DispatchSource.makeTimerSource(flags: .strict, queue: queue)
+            babyTimer.setEventHandler(qos: .unspecified) {
+                [weak self] in
+                guard let self else { return }
+                self.trigger()
+            }
+			_timer = babyTimer
+			return babyTimer
 		}
-		return timer
+		return existingTimer
 	}
 	
 	/// 是否正运行
 	private var isTicking = false
 	/// 定时器是否有效
-	private var isValid: Bool { timerSource != nil }
+	private var isValid: Bool { _timer != nil }
 	
 	let timeInterval: DispatchTimeInterval
 	let queue: DispatchQueue
-	let closure: GCDTimerClosure
+	let tickTock: TickTock
 	
 	/// 定时器初始化方法
 	/// - Parameters:
 	///   - timeInterval: 时间间隔
 	///   - queue: 执行的队列
-	///   - closure: 回调闭包
-	init(timeInterval: DispatchTimeInterval, queue: DispatchQueue = .main, closure: @escaping GCDTimerClosure) {
+	///   - tickTock: 回调闭包
+	init(timeInterval: DispatchTimeInterval, queue: DispatchQueue = .main, tickTock: @escaping TickTock) {
 		self.timeInterval = timeInterval
 		self.queue = queue
-		self.closure = closure
+		self.tickTock = tickTock
 	}
 	
 	/// 创建Timer | 根据延迟时间启动定时器⏲
@@ -51,23 +55,23 @@ final class GCDTimer {
 	///   - timeInterval: 时间间隔 | .never代表只执行一次
 	///   - delay: 延迟时间
 	///   - queue: 调用队列
-	///   - closure: 回调方法
+	///   - tickTock: 回调方法
 	/// - Returns: 定时器实列
 	@discardableResult
 	static func scheduledTimer(
 		timeInterval: DispatchTimeInterval = .never,
 		delay: DispatchTime = .now(),
 		queue: DispatchQueue = .main,
-		closure: @escaping GCDTimerClosure)
+        tickTock: @escaping TickTock)
 	-> GCDTimer {
-		let timer = self.init(timeInterval: timeInterval, queue: queue, closure: closure)
+		let timer = self.init(timeInterval: timeInterval, queue: queue, tickTock: tickTock)
 		timer.fire(delay)
 		return timer
 	}
 	
 	/// 定时器调用方法
-	private func tickTock() {
-		closure(self)
+	private func trigger() {
+		tickTock(self)
 		// 只执行一次
 		if timeInterval == .never {
 			invalidate()
@@ -77,8 +81,8 @@ final class GCDTimer {
 	/// 销毁定时器
 	func invalidate() {
 		if isValid {
-			validTimer.cancel()
-			timerSource = .none
+			timer.cancel()
+			_timer = .none
 			isTicking = false
 		}
 	}
@@ -86,7 +90,7 @@ final class GCDTimer {
 	/// 挂起定时器
 	func suspend() {
 		if isValid && isTicking {
-			validTimer.suspend()
+			timer.suspend()
 			isTicking = false
 		}
 	}
@@ -94,7 +98,7 @@ final class GCDTimer {
 	/// 继续执行定时器
 	func resume() {
 		if isValid && !isTicking {
-			validTimer.resume()
+			timer.resume()
 			isTicking = true
 		}
 	}
@@ -102,7 +106,7 @@ final class GCDTimer {
 	/// 启动定时器
 	/// - Parameter delay: 延迟时间
 	func fire(_ delay: DispatchTime = .now()) {
-		validTimer.schedule(deadline: delay, repeating: timeInterval)
+		timer.schedule(deadline: delay, repeating: timeInterval)
 		resume()
 	}
 	

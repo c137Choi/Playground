00b037ae17c6ebb97acff525a42fb0bc73cf5cad
Choi <365473321@qq.com>
2025-04-11 10:48:12 +0800

    NWPathMonitor+Rx优化

--- RxPlayground/RxExtensions/NWPathMonitor+Rx.swift
+++ RxPlayground/RxExtensions/NWPathMonitor+Rx.swift
@@ -13,33 +13,34 @@ import RxCocoa
 extension NWPathMonitor: @retroactive ReactiveCompatible {}
 extension Reactive where Base == NWPathMonitor {
     
-    private func start() {
-        let queue = DispatchQueue.global(qos: .background)
-        base.start(queue: queue)
-    }
-    
-    private func cancel() {
-        base.cancel()
-    }
-    
-    /// .satisfied NWPath
-    /// - Returns: 蜂窝网络返回空
-    private func satisfiedPath(_ path: NWPath) -> NWPath? {
-        (path.status == .satisfied ? path : nil).flatMap { path in
-            guard path.usesInterfaceType(.wifi) || path.usesInterfaceType(.wiredEthernet) else { return nil }
-            return path
+    var satisfiedEthernetPath: Observable<NWPath?> {
+        func satisfiedEthernetPath(_ path: NWPath) -> NWPath? {
+            (path.status == .satisfied ? path : nil).flatMap { path in
+                guard path.usesInterfaceType(.wifi) || path.usesInterfaceType(.wiredEthernet) else { return nil }
+                return path
+            }
         }
+        return currentPath.map(satisfiedEthernetPath)
     }
     
-    var satisfiedPath: Observable<NWPath?> {
-        Observable.create { observer in
-            base.pathUpdateHandler = { path in
+    var currentPath: Observable<NWPath> {
+        Observable.create {
+            [weak monitor = base] observer in
+            guard let monitor else {
+                observer.onCompleted()
+                return Disposables.create()
+            }
+            observer.onNext(monitor.currentPath)
+            monitor.pathUpdateHandler = { path in
                 observer.onNext(path)
             }
-            return Disposables.create()
+            let queue = DispatchQueue(label: "com.nw.path.monitor", qos: .userInitiated)
+            monitor.start(queue: queue)
+            return Disposables.create {
+                [weak monitor] in monitor?.cancel()
+            }
         }
-        .startWith(base.currentPath)
-        .map(satisfiedPath)
-        .do(onSubscribed: start, onDispose: cancel)
+        .distinctUntilChanged()
+        .share(replay: 1, scope: .whileConnected)
     }
 }

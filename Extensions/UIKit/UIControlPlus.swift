98d4a86ebbb0c30277afa3a14ecc29819696a104
Choi <365473321@qq.com>
2025-06-10 18:01:36 +0800

    新增：UIControl.State描述

--- Extensions/UIKit/UIControlPlus.swift
+++ Extensions/UIKit/UIControlPlus.swift
@@ -14,3 +14,35 @@ extension UIControl {
         set(disabled) { isEnabled = !disabled }
     }
 }
+
+extension UIControl.State: @retroactive CustomDebugStringConvertible {
+    
+    public var debugDescription: String {
+        if isEmpty { return "empty" }
+        var desc = ""
+        if contains(.normal) {
+            desc += "normal, "
+        }
+        if contains(.highlighted) {
+            desc += "highlighted, "
+        }
+        if contains(.disabled) {
+            desc += "disabled, "
+        }
+        if contains(.selected) {
+            desc += "selected, "
+        }
+        if contains(.focused) {
+            desc += "focused, "
+        }
+        if contains(.application) {
+            desc += "application, "
+        }
+        if contains(.reserved) {
+            desc += "reserved, "
+        }
+        /// 移除逗号+空格
+        desc.removeLast(2)
+        return desc
+    }
+}

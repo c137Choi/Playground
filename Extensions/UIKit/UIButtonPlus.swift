160c5cf522b232e361b3a82fe16ac6d80de07962
Choi <365473321@qq.com>
2025-09-01 17:16:24 +0800

    清理UIButton过时的属性

--- Extensions/UIKit/UIButtonPlus.swift
+++ Extensions/UIKit/UIButtonPlus.swift
@@ -8,33 +8,12 @@
 import UIKit
 
 extension UIButton {
-	
-	enum ImagePlacement: Int {
-		case top
-		case left
-		case bottom
-		case right
-		
-		@available(iOS 13.0, *)
-		fileprivate var configTranslation: NSDirectionalRectEdge {
-			switch self {
-			case .top: return .top
-			case .left: return .leading
-			case .bottom: return .bottom
-			case .right: return .trailing
-			}
-		}
-	}
-	
-	enum Associated {
-		@UniqueAddress static var imagePlacement
-		@UniqueAddress static var imagePadding
-		@UniqueAddress static var useBackgroundImageSize
-	}
-}
-
-extension UIButton {
-	
+    
+    enum Associated {
+        @UniqueAddress static var imagePlacement
+        @UniqueAddress static var imagePadding
+    }
+    
     convenience init(image: UIImage?) {
         self.init(frame: .zero)
         setImage(image, for: .normal)
@@ -52,103 +31,52 @@ extension UIButton {
         titleLabel?.font = font
     }
     
-	open override func setNeedsLayout() {
-		super.setNeedsLayout()
-		setupImageTitleEdgeInsets()
-	}
+    var titleFont: UIFont? {
+        get { titleLabel?.font }
+        set { titleLabel?.font = newValue }
+    }
 	
-	/// 是否使用背景图片尺寸作为按钮固有尺寸
-	var useBackgroundImageSize: Bool {
-        get {
-            associated(Bool.self, self, Associated.useBackgroundImageSize).or(false)
-        }
-		set {
-			setAssociatedObject(self, Associated.useBackgroundImageSize, newValue, .OBJC_ASSOCIATION_ASSIGN)
-			invalidateIntrinsicContentSize()
-		}
-	}
-	
-	/// 图片位置
-	var imagePlacement: ImagePlacement {
-		get {
-            associated(Int.self, self, Associated.imagePlacement).flatMap(ImagePlacement.init, fallback: .left)
-		}
-		set {
-			setAssociatedObject(self, Associated.imagePlacement, newValue.rawValue, .OBJC_ASSOCIATION_ASSIGN)
-			setupImageTitleEdgeInsets()
-		}
-	}
-	
-	/// Image-Title间距(大于等于0; 最好是偶数,否则按钮显示可能会有小小误差<iOS中像素对齐导致的问题>)
-	var imagePadding: CGFloat {
-        get {
-            associated(CGFloat.self, self, Associated.imagePadding).or(0.0)
-        }
-		set {
-			assert(newValue >= 0, "A sane person would never do that.")
-			setAssociatedObject(self, Associated.imagePadding, newValue, .OBJC_ASSOCIATION_ASSIGN)
-			setupImageTitleEdgeInsets()
-		}
-	}
-	
-	var backgroundImageView: UIImageView? {
-        subviews.lazy.as(UIImageView.self).first { imgView in
-            imgView.image == currentBackgroundImage
-        }
-	}
-	
-	var titleFont: UIFont? {
-		get { titleLabel?.font }
-		set { titleLabel?.font = newValue }
-	}
-	
-	var imageSize: CGSize { currentImage?.size ?? .zero }
-	var titleSize: CGSize {
+    private var imageWidth: CGFloat { imageSize.width }
+    private var imageHeight: CGFloat { imageSize.height }
+    private var titleWidth: CGFloat { titleSize.width }
+    private var titleHeight: CGFloat { titleSize.height }
+    
+    private var imageSize: CGSize {
+        currentImage.or(.zero, map: \.size)
+    }
+    
+	private var titleSize: CGSize {
 		/// 适配iOS14,否则此属性会按照字体的Font返回一个值,从而影响intrinsicContentSize的计算
-		guard let titleLabel = titleLabel, titleLabel.text != .none else {
-			return .zero
-		}
+		guard let titleLabel = titleLabel, titleLabel.text != .none else { return .zero }
 		let additionalWidth = UIAccessibility.isBoldTextEnabled ? 1.5 : 0
 		let additionalSize = CGSize(width: additionalWidth, height: 0)
 		return titleLabel.intrinsicContentSize + additionalSize
 	}
-	var imageWidth: CGFloat { imageSize.width }
-	var imageHeight: CGFloat { imageSize.height }
-	var titleWidth: CGFloat { titleSize.width }
-	var titleHeight: CGFloat { titleSize.height }
-	
-	/// 可以根据contentEdgeInsets自动适配自身大小
-	open override var intrinsicContentSize: CGSize {
-
-		let backgroundImageSize = currentBackgroundImage?.size ?? .zero
-		var regularSize: CGSize {
-			if imageSize == .zero {
-				return titleSize + contentEdgeInsets
-			} else if titleSize == .zero {
-				return imageSize + contentEdgeInsets
-			} else {
-				// 初始化size
-				var size = CGSize.zero
-				// 计算宽高
-				switch imagePlacement  {
-				case .top, .bottom:
-					size.width = max(imageWidth, titleWidth)
-					size.height = imageHeight + imagePadding + titleHeight
-				case .left, .right:
-					size.width = imageWidth + imagePadding + titleWidth
-					size.height = max(imageHeight, titleHeight)
-				}
-				return size + contentEdgeInsets
-			}
-		}
-
-		return useBackgroundImageSize ? backgroundImageSize : regularSize
-	}
+    
+    /// 图片位置
+    private var imagePlacement: UITextLayoutDirection {
+        get {
+            associated(Int.self, self, Associated.imagePlacement).flatMap(UITextLayoutDirection.init, fallback: .left)
+        }
+        set {
+            setAssociatedObject(self, Associated.imagePlacement, newValue.rawValue, .OBJC_ASSOCIATION_ASSIGN)
+            setupImageTitleEdgeInsets()
+        }
+    }
+    
+    /// Image-Title间距(大于等于0; 最好是偶数,否则按钮显示可能会有小小误差<iOS中像素对齐导致的问题>)
+    private var imagePadding: CGFloat {
+        get {
+            associated(CGFloat.self, self, Associated.imagePadding).or(0.0)
+        }
+        set {
+            assert(newValue >= 0, "A sane person would never do that.")
+            setAssociatedObject(self, Associated.imagePadding, newValue, .OBJC_ASSOCIATION_ASSIGN)
+            setupImageTitleEdgeInsets()
+        }
+    }
 	
 	private func setupImageTitleEdgeInsets() {
-		defer {
-			invalidateIntrinsicContentSize()
-		}
 		guard imageSize != .zero && titleSize != .zero else { return }
 		
 		var imageInsets = UIEdgeInsets.zero
@@ -182,13 +110,13 @@ extension UIButton {
 			
 		case (.center, .center):
 			switch imagePlacement {
-			case .top:
+			case .up:
 				imageInsets = UIEdgeInsets(top: -titleHeightWithPadding, left: 0, bottom: 0, right: -titleWidth)
 				titleInsets = UIEdgeInsets(top: 0, left: -imageWidth, bottom: -imageHeightWithPadding, right: 0)
 			case .left:
 				imageInsets = UIEdgeInsets(top: 0, left: -halfPadding, bottom: 0, right: halfPadding)
 				titleInsets = UIEdgeInsets(top: 0, left: halfPadding, bottom: 0, right: -halfPadding)
-			case .bottom:
+			case .down:
 				imageInsets = UIEdgeInsets(top: 0, left: 0, bottom: -titleHeightWithPadding, right: -titleWidth)
 				titleInsets = UIEdgeInsets(top: -imageHeightWithPadding, left: -imageWidth, bottom: 0, right: 0)
 			case .right:
@@ -196,59 +124,67 @@ extension UIButton {
 				let titleOffset = imageWidth + halfPadding
 				imageInsets = UIEdgeInsets(top: 0, left: imageOffset, bottom: 0, right: -imageOffset)
 				titleInsets = UIEdgeInsets(top: 0, left: -titleOffset, bottom: 0, right: titleOffset)
-			}
+            @unknown default:
+                fatalError("UNKOWN CASE")
+            }
 		case (.center, .left):
 			switch imagePlacement {
-			case .top:
+			case .up:
 				imageInsets = UIEdgeInsets(top: -titleHeightWithPadding, left: imageHOffset, bottom: 0, right: 0)
 				titleInsets = UIEdgeInsets(top: 0, left: -imageWidth + titleHOffset, bottom: -imageHeightWithPadding, right: -titleHOffset)
 			case .left:
 				titleInsets = UIEdgeInsets(top: 0, left: imagePadding, bottom: 0, right: -imagePadding)
-			case .bottom:
+			case .down:
 				imageInsets = UIEdgeInsets(top: 0, left: imageHOffset, bottom: -titleHeightWithPadding, right: -titleWidth)
 				titleInsets = UIEdgeInsets(top: -imageHeightWithPadding, left: -imageWidth + titleHOffset, bottom: 0, right: 0)
 			case .right:
 				imageInsets = UIEdgeInsets(top: 0, left: titleWidthWithPadding, bottom: 0, right: -titleWidthWithPadding)
 				titleInsets = UIEdgeInsets(top: 0, left: -imageWidth, bottom: 0, right: imageWidth)
-			}
+            @unknown default:
+                fatalError("UNKOWN CASE")
+            }
 		case (.center, .right):
 			switch imagePlacement {
-			case .top:
+			case .up:
 				imageInsets = UIEdgeInsets(top: -titleHeightWithPadding, left: 0, bottom: 0, right: -titleWidth + imageHOffset)
 				titleInsets = UIEdgeInsets(top: 0, left: -imageWidth, bottom: -imageHeightWithPadding, right: titleHOffset)
 			case .left:
 				imageInsets = UIEdgeInsets(top: 0, left: -imagePadding, bottom: 0, right: imagePadding)
-			case .bottom:
+			case .down:
 				imageInsets = UIEdgeInsets(top: 0, left: 0, bottom: -titleHeightWithPadding, right: -titleWidth + imageHOffset)
 				titleInsets = UIEdgeInsets(top: -imageHeightWithPadding, left: -imageWidth, bottom: 0, right: titleHOffset)
 			case .right:
 				imageInsets = UIEdgeInsets(top: 0, left: titleWidth, bottom: 0, right: -titleWidth)
 				titleInsets = UIEdgeInsets(top: 0, left: -imageWidthWithPadding, bottom: 0, right: imageWidthWithPadding)
-			}
+            @unknown default:
+                fatalError("UNKOWN CASE")
+            }
 		case (.top, .left):
 			switch imagePlacement {
-			case .top:
+			case .up:
 				imageInsets = UIEdgeInsets(top: 0, left: imageHOffset, bottom: 0, right: 0)
 				titleInsets = UIEdgeInsets(top: imageHeightWithPadding, left: -imageWidth + titleHOffset, bottom: 0, right: 0)
 			case .left:
 				imageInsets = UIEdgeInsets(top: imageVOffset, left: 0, bottom: 0, right: 0)
 				titleInsets = UIEdgeInsets(top: titleVOffset, left: imagePadding, bottom: 0, right: -imagePadding)
-			case .bottom:
+			case .down:
 				imageInsets = UIEdgeInsets(top: titleHeightWithPadding, left: imageHOffset, bottom: -titleHeightWithPadding, right: 0)
 				titleInsets = UIEdgeInsets(top: 0, left: -imageWidth + titleHOffset, bottom: 0, right: 0)
 			case .right:
 				imageInsets = UIEdgeInsets(top: imageVOffset, left: titleWidthWithPadding, bottom: 0, right: -titleWidthWithPadding)
 				titleInsets = UIEdgeInsets(top: titleVOffset, left: -imageWidth, bottom: 0, right: 0)
-			}
+            @unknown default:
+                fatalError("UNKOWN CASE")
+            }
 		case (.top, .center):
 			switch imagePlacement {
-			case .top:
+			case .up:
 				imageInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: -titleWidth)
 				titleInsets = UIEdgeInsets(top: imageHeightWithPadding, left: -imageWidth, bottom: 0, right: 0)
 			case .left:
 				imageInsets = UIEdgeInsets(top: imageVOffset, left: -halfPadding, bottom: 0, right: halfPadding)
 				titleInsets = UIEdgeInsets(top: titleVOffset, left: halfPadding, bottom: 0, right: -halfPadding)
-			case .bottom:
+			case .down:
 				imageInsets = UIEdgeInsets(top: titleHeightWithPadding, left: 0, bottom: -titleHeightWithPadding, right: -titleWidth)
 				titleInsets = UIEdgeInsets(top: 0, left: -imageWidth, bottom: 0, right: 0)
 			case .right:
@@ -256,46 +192,52 @@ extension UIButton {
 				let titleOffset = imageWidth + halfPadding
 				imageInsets = UIEdgeInsets(top: imageVOffset, left: imageOffset, bottom: 0, right: -imageOffset)
 				titleInsets = UIEdgeInsets(top: titleVOffset, left: -titleOffset, bottom: 0, right: titleOffset)
-			}
+            @unknown default:
+                fatalError("UNKOWN CASE")
+            }
 		case (.top, .right):
 			switch imagePlacement {
-			case .top:
+			case .up:
 				imageInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: -titleWidth + imageHOffset)
 				titleInsets = UIEdgeInsets(top: imageHeightWithPadding, left: -imageWidthWithPadding, bottom: 0, right: titleHOffset)
 			case .left:
 				imageInsets = UIEdgeInsets(top: imageVOffset, left: -imagePadding, bottom: 0, right: imagePadding)
 				titleInsets = UIEdgeInsets(top: titleVOffset, left: 0, bottom: 0, right: 0)
-			case .bottom:
+			case .down:
 				imageInsets = UIEdgeInsets(top: titleHeightWithPadding, left: 0, bottom: -titleHeightWithPadding, right: -titleWidth + imageHOffset)
 				titleInsets = UIEdgeInsets(top: 0, left: -imageWidth, bottom: 0, right: titleHOffset)
 			case .right:
 				imageInsets = UIEdgeInsets(top: imageVOffset, left: titleWidth, bottom: 0, right: -titleWidth)
 				titleInsets = UIEdgeInsets(top: titleVOffset, left: -imageWidthWithPadding, bottom: 0, right: imageWidthWithPadding)
-			}
+            @unknown default:
+                fatalError("UNKOWN CASE")
+            }
 		case (.bottom, .left):
 			switch imagePlacement {
-			case .top:
+			case .up:
 				imageInsets = UIEdgeInsets(top: -titleHeightWithPadding, left: imageHOffset, bottom: titleHeightWithPadding, right: 0)
 				titleInsets = UIEdgeInsets(top: 0, left: -imageWidth + titleHOffset, bottom: 0, right: 0)
 			case .left:
 				imageInsets = UIEdgeInsets(top: 0, left: 0, bottom: imageVOffset, right: 0)
 				titleInsets = UIEdgeInsets(top: 0, left: imagePadding, bottom: titleVOffset, right: -imagePadding)
-			case .bottom:
+			case .down:
 				imageInsets = UIEdgeInsets(top: 0, left: imageHOffset, bottom: 0, right: 0)
 				titleInsets = UIEdgeInsets(top: 0, left: -imageWidth + titleHOffset, bottom: imageHeightWithPadding, right: 0)
 			case .right:
 				imageInsets = UIEdgeInsets(top: 0, left: titleWidthWithPadding, bottom: imageVOffset, right: -titleWidthWithPadding)
 				titleInsets = UIEdgeInsets(top: 0, left: -imageWidth, bottom: titleVOffset, right: imageWidth)
-			}
+            @unknown default:
+                fatalError("UNKOWN CASE")
+            }
 		case (.bottom, .center):
 			switch imagePlacement {
-			case .top:
+			case .up:
 				imageInsets = UIEdgeInsets(top: 0, left: 0, bottom: titleHeightWithPadding, right: -titleWidth)
 				titleInsets = UIEdgeInsets(top: imageHeightWithPadding, left: -imageWidth, bottom: 0, right: 0)
 			case .left:
 				imageInsets = UIEdgeInsets(top: 0, left: -halfPadding, bottom: imageVOffset, right: halfPadding)
 				titleInsets = UIEdgeInsets(top: 0, left: halfPadding, bottom: titleVOffset, right: -halfPadding)
-			case .bottom:
+			case .down:
 				imageInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: -titleWidth)
 				titleInsets = UIEdgeInsets(top: 0, left: -imageWidth, bottom: imageHeightWithPadding, right: 0)
 			case .right:
@@ -303,60 +245,30 @@ extension UIButton {
 				let titleOffset = imageWidth + halfPadding
 				imageInsets = UIEdgeInsets(top: 0, left: imageOffset, bottom: imageVOffset, right: -imageOffset)
 				titleInsets = UIEdgeInsets(top: 0, left: -titleOffset, bottom: titleVOffset, right: titleOffset)
-			}
+            @unknown default:
+                fatalError("UNKOWN CASE")
+            }
 		case (.bottom, .right):
 			switch imagePlacement {
-			case .top:
+			case .up:
 				imageInsets = UIEdgeInsets(top: -titleHeightWithPadding, left: 0, bottom: titleHeightWithPadding, right: -titleWidth + imageHOffset)
 				titleInsets = UIEdgeInsets(top: 0, left: -imageWidth, bottom: 0, right: titleHOffset)
 			case .left:
 				imageInsets = UIEdgeInsets(top: 0, left: -imagePadding, bottom: imageVOffset, right: imagePadding)
 				titleInsets = UIEdgeInsets(top: 0, left: 0, bottom: titleVOffset, right: 0)
-			case .bottom:
+			case .down:
 				imageInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: -titleWidth + imageHOffset)
 				titleInsets = UIEdgeInsets(top: -imageHeightWithPadding, left: -imageWidth, bottom: imageHeightWithPadding, right: titleHOffset)
 			case .right:
 				imageInsets = UIEdgeInsets(top: 0, left: titleWidth, bottom: imageVOffset, right: -titleWidth)
 				titleInsets = UIEdgeInsets(top: 0, left: -imageWidthWithPadding, bottom: titleVOffset, right: imageWidthWithPadding)
-			}
+            @unknown default:
+                fatalError("UNKOWN CASE")
+            }
 		default:
 			break
 		}
-		
 		imageEdgeInsets = imageInsets
 		titleEdgeInsets = titleInsets
-	}
-	
-	@available(iOS 15.0, *)
-	private func setConfigurationUpdateHandlerIfNeeded() {
-		/// Try to assign any value to configurationUpdateHandler on a button
-		/// created with the old API will cause a run-time error with the following message.
-		///
-		/// Terminating app due to uncaught exception 'NSInternalInconsistencyException',
-		/// reason: 'Invalid parameter not satisfying: configuration != nil'
-		if configuration != nil, configurationUpdateHandler == nil {
-			configurationUpdateHandler = { button in
-				var config = button.configuration ?? .plain()
-				config.contentInsets = button.contentEdgeInsets.directionalEdgeInsets
-				config.imagePadding = button.imagePadding
-				config.imagePlacement = button.imagePlacement.configTranslation
-				if let title = button.title(for: button.state) {
-					var titleAttributes = AttributeContainer()
-					titleAttributes.font = button.titleLabel?.font
-					titleAttributes.foregroundColor = button.titleColor(for: button.state)
-					config.attributedTitle = AttributedString(title, attributes: titleAttributes)
-				}
-				if let attributedTitle = button.attributedTitle(for: button.state) {
-					config.attributedTitle = AttributedString(attributedTitle)
-				}
-				config.image = button.image(for: button.state)
-				config.background.image = button.backgroundImage(for: button.state)
-				config.background.backgroundColor = button.backgroundColor
-				config.baseBackgroundColor = button.backgroundColor
-				
-				button.configuration = config
-			}
-		}
-		setNeedsUpdateConfiguration()
 	}
 }

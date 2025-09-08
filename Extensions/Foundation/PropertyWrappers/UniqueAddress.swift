94f0a4ea94a8afb21a72bc9f37d77dbbbdbdad74
Choi <365473321@qq.com>
2025-09-08 11:49:13 +0800

    调整PropertyWrapper目录结构

---
+++ Extensions/Foundation/PropertyWrappers/UniqueAddress.swift
@@ -0,0 +1,15 @@
+//
+//  UniqueAddress.swift
+//  KnowLED
+//
+//  Created by Choi on 2025/9/8.
+//
+
+/// https://github.com/atrick/swift-evolution/blob/diagnose-implicit-raw-bitwise/proposals/nnnn-implicit-raw-bitwise-conversion.md#associated-object-string-keys
+@propertyWrapper
+public class UniqueAddress {
+    public init() {}
+    public var wrappedValue: UnsafeRawPointer {
+        UnsafeRawPointer(Unmanaged.passUnretained(self).toOpaque())
+    }
+}

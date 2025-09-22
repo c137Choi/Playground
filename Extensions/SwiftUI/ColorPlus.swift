49b76df66c33829be807b73900a3fa5309063e67
Choi <365473321@qq.com>
2025-09-22 17:31:37 +0800

    新增：Color扩展方法

---
+++ Extensions/SwiftUI/ColorPlus.swift
@@ -0,0 +1,16 @@
+//
+//  ColorPlus.swift
+//  KnowLED
+//
+//  Created by Choi on 2025/9/22.
+//
+
+import UIKit
+import SwiftUI
+
+extension Color {
+    
+    var uiColor: UIColor {
+        UIColor(self)
+    }
+}

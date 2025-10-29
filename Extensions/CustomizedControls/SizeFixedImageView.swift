900c3b93316e3b213f465b001c6431e1f2aa921b
Choi <365473321@qq.com>
2025-10-29 09:04:19 +0800

    完善SizeFixedImageView

--- Extensions/CustomizedControls/SizeFixedImageView.swift
+++ Extensions/CustomizedControls/SizeFixedImageView.swift
@@ -5,6 +5,8 @@
 //  Created by Choi on 2025/10/11.
 //
 
+import UIKit
+
 final class SizeFixedImageView: UIImageView {
     
     /// 固定尺寸
@@ -18,7 +20,7 @@ final class SizeFixedImageView: UIImageView {
     /// - Parameters:
     ///   - fixedSize: 固定尺寸
     ///   - image: 图片
-    convenience init(_ fixedSize: CGSize, image: UIImage? = nil) {
+    convenience init(size fixedSize: CGSize, image: UIImage? = nil) {
         self.init(image: image)
         self.fixedSize = fixedSize
         self.bounds.size = fixedSize

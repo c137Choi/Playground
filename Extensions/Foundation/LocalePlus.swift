9634757c43c6f95f4a32d5459ccca3cc7b051890
Choi <365473321@qq.com>
2026-01-13 14:48:09 +0800

    添加通用字符串本地化方法

--- Extensions/Foundation/LocalePlus.swift
+++ Extensions/Foundation/LocalePlus.swift
@@ -7,7 +7,23 @@
 import Foundation
 
 extension Locale {
+    static var application = Locale.current
+    
     static let chineseSimplified = Locale(identifier: "zh_CN")
     static let chineseTraditional = Locale(identifier: "zh-Hant_CN")
     static let enUS = Locale(identifier: "en_US")
+}
+
+func localized(
+    _ localized: String.LocalizationValue,
+    table: String? = nil,
+    locale: Locale = .application,
+    comment: StaticString? = nil) -> String
+{
+    if #available(iOS 16.0, *) {
+        let resource = LocalizedStringResource(localized, table: table, locale: locale, comment: comment)
+        return String(localized: resource)
+    } else {
+        return String(localized: localized, table: table, locale: locale, comment: comment)
+    }
 }

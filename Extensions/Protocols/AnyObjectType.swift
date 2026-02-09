d97d3525eeed8ac3d927d87a1d4cdaa737f6a816
Choi <365473321@qq.com>
2026-02-09 18:24:49 +0800

    新增：AnyObjectType

---
+++ Extensions/Protocols/AnyObjectType.swift
@@ -0,0 +1,16 @@
+//
+//  AnyObjectType.swift
+//  KnowLED
+//
+//  Created by Choi on 2026/2/9.
+//
+
+import Foundation
+
+protocol AnyObjectType: AnyObject {}
+
+extension NSObject: AnyObjectType {}
+
+extension AnyObjectType {
+    
+}

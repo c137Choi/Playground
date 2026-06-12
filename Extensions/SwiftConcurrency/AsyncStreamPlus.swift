82e28c4449643d19ce006f5040a93ab38ff7c7d8
Choi <365473321@qq.com>
2026-06-12 16:49:53 +0800

    新增AsyncStream扩展方法

--- Extensions/SwiftConcurrency/AsyncStreamPlus.swift
+++ Extensions/SwiftConcurrency/AsyncStreamPlus.swift
@@ -11,14 +11,18 @@ import RxSwift
 
 extension AsyncStream where Element: FixedWidthInteger {
     
+    nonisolated static func timer(milliseconds: Int) -> AsyncStream<Element> {
+        timer(period: .milliseconds(milliseconds))
+    }
+    
     /// AsyncStream定时器
     /// - Parameters:
     ///   - dueTime: 执行前等待时间
     ///   - period: 定时器间隔
     /// - Returns: AsyncStream<Element>
-    nonisolated static func timer(dueTime: RxTimeInterval = 0, period: RxTimeInterval?) -> AsyncStream<Element> {
+    nonisolated static func timer(period: RxTimeInterval?) -> AsyncStream<Element> {
         SerialDispatchQueueScheduler(qos: .userInitiated).transform { scheduler in
-            Observable<Element>.timer(dueTime, period: period, scheduler: scheduler)
+            Observable<Element>.timer(0, period: period, scheduler: scheduler)
                 .asInfallible(onErrorJustReturn: 0)
                 .values
         }

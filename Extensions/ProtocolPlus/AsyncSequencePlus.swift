134ef0d088d42132ab6dacf5bad1a3c94d4b3e7c
Choi <365473321@qq.com>
2026-06-18 14:11:14 +0800

    新增AsyncSequence扩展方法

--- Extensions/ProtocolPlus/AsyncSequencePlus.swift
+++ Extensions/ProtocolPlus/AsyncSequencePlus.swift
@@ -7,13 +7,23 @@
 
 import Foundation
 
-extension AsyncSequence {
+nonisolated extension AsyncSequence {
     
     /// 来源: https://www.hackingwithswift.com/quick-start/concurrency/how-to-convert-an-asyncsequence-into-a-sequence
     /// Tip: Because we’ve made collect() use rethrows, you only need to call it using try if the call to reduce() would normally throw, so if you have an async sequence that doesn’t throw errors you can skip try entirely.
     func collect() async rethrows -> [Element] {
-        try await reduce(into: [Element].empty) { array, next in
-            array.append(next)
+        try await reduce(into: [Element].empty) { array, element in
+            array.append(element)
+        }
+    }
+}
+
+nonisolated extension AsyncSequence where Element: OptionalConvertible {
+    
+    func collect() async rethrows -> [Element.Wrapped] where Element: OptionalConvertible {
+        try await reduce(into: [Element.Wrapped].empty) { array, element in
+            guard let unwrapped = element.optionalValue else { return }
+            array.append(unwrapped)
         }
     }
 }

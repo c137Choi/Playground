cf72bb6b29aaf1e53b8552c03da82efed901d670
Choi <365473321@qq.com>
2026-07-06 17:22:17 +0800

    新增UUID扩展属性

--- Extensions/Foundation/UUIDPlus.swift
+++ Extensions/Foundation/UUIDPlus.swift
@@ -13,4 +13,13 @@ nonisolated extension UUID {
     static var new: UUID {
         UUID()
     }
+    
+    var data: Data {
+        Data(bytes)
+    }
+    
+    var bytes: [UInt8] {
+        let (b0, b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12, b13, b14, b15) = uuid
+        return [b0, b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12, b13, b14, b15]
+    }
 }
